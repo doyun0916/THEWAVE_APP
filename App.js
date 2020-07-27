3eb61a38267ea590605020198e3d61537211094e
doyun0916 <doyun0916@naver.com>
2020-07-28 03:26:43 +0900

    main_page_w/o onfocus

--- App.js
+++ App.js
@@ -7,42 +7,47 @@
  */
 
 import React from 'react';
-import { StyleSheet, View, Text, Image, ImageBackground } from 'react-native';
+import { StyleSheet, View, Text, Image, ImageBackground, TouchableOpacity, TextInput, TouchableHighlight } from 'react-native';
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <ImageBackground source={require('./icon/background.jpg')} style={styles.image}>
           <View style={styles.header}>
           </View>
           <View style={styles.logo}>
-            <Image style={{width: "55%", height: "55%", resizeMode: 'contain', borderRadius:100 }} source={require('./icon/logo.png')}/>
+            <Image style={{width: "45%", height: "45%", resizeMode: 'contain', borderRadius:100 }} source={require('./icon/logo.png')}/>
           </View>
           <View style={styles.login}>
+            <TextInput placeholder='ID' style={styles.id} />
+            <TextInput placeholder='PASSWORD' style={styles.pw} />
+            <TouchableOpacity style={styles.login_bt}>
+              <ImageBackground source={require('./icon/background.jpg')} imageStyle={{borderRadius: 30}} style={styles.bgImage}>
+                <Text style={styles.login_char}>로그인</Text>
+              </ImageBackground>
+            </TouchableOpacity>
           </View>
           <View style={styles.footer}>
+            <TouchableOpacity>
+              <Text style={{color: 'gray', marginTop: 28, marginRight: 35, alignSelf: 'flex-end', fontWeight:'bold'}}>CONTACT</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={{color: 'gray', marginTop: -18, marginLeft: 35, alignSelf: 'flex-start', fontWeight: 'bold'}}>회원가입</Text>
+            </TouchableOpacity>
           </View>
-        </ImageBackground>
       </View>
     );
   }
 }
+export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  },
-  image: {
     flex: 1,
-    resizeMode: "contain",
-    width: "100%",
-    height: "100%",
-    justifyContent: "center",
-    alignItems: 'stretch'
+    backgroundColor: 'white'
   },
   header: {
-    flex: 0.7
+    flex: 0.3
   },
   logo: {
     flex: 1.5,
@@ -52,8 +57,50 @@ const styles = StyleSheet.create({
   },
   login: {
     flex: 2,
+    alignItems: 'center'
   },
   footer: {
     flex: 0.7,
+  },
+  id: {
+    marginTop: 20,
+    width: 270,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: 30,
+    borderWidth: 2.5,
+    borderColor: 'lightgray',
+    paddingLeft: 15
+  },
+  pw: {
+    marginTop: 5,
+    width: 270,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: 30,
+    borderWidth: 2.5,
+    borderColor: 'lightgray',
+    paddingLeft: 15
+  },
+  login_bt: {
+    marginTop: 30,
+    width: 270,
+    height: 40
+  },
+  bgImage: {
+    justifyContent: 'center',
+    resizeMode: "cover",
+    width: "100%",
+    height: "100%",
+    alignItems: 'center',
+  },
+  login_char: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight:'bold',
   }
 });
